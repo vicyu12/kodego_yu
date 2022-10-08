481504a2237e02830723dd64e455393479460a9c
Victor Yu Jr <vicyu12@gmail.com>
2022-10-08 15:49:34 +0800

    activity complete

---
+++ src/main/kotlin/activity_01c/Main.kt
@@ -0,0 +1,38 @@
+package activity_01c
+
+/*
+You are tasked to automate an inventory system for a grocery.
+
+Identify the items that can be bought in a grocery store.
+
+After listing the different items, identify the characteristics of the items.
+
+You are also tasked to group the items in categories to help manage the grocery.  Use the proper data types.
+ */
+
+fun main() {
+    val itemName1: String = "Chicken Nuggets"
+    var itemQuantity1: Int = 25
+    var itemAvailability1: Boolean = true
+    var itemCharacteristics1: String? = null
+    val itemCategory1: String = "Frozen Food"
+    val itemBrand1: String = "PureFoods"
+    val itemModel1: String = "Crazy Cut Nuggets"
+
+    val itemName2: String = "Corned Beef"
+    var itemQuantity2: Int = 5
+    var itemAvailability2: Boolean = true
+    var itemCharacteristics2: String? = "Dented"
+    val itemCategory2: String = "Canned Food"
+    val itemBrand2: String = "PureFoods"
+    val itemModel2: String = "Chili Garlic"
+
+    val itemName3: String = "Bathroom Tissue"
+    var itemQuantity3: Int = 10
+    var itemAvailability3: Boolean = true
+    var itemCharacteristics3: String? = "Sealed"
+    val itemCategory3: String = "Paper Goods"
+    val itemBrand3: String = "Sanicare"
+    val itemModel3: String = "EcoLayers"
+
+}
