e9051e0b7d08a59060feff406bbc6bc65a538216
Victor Yu Jr <vicyu12@gmail.com>
2022-10-08 09:38:06 +0800

    Initial Commit Activity01A

---
+++ src/main/kotlin/activity_01a/Main.kt
@@ -0,0 +1,134 @@
+package activity_01a
+/*
+You are tasked to create a directory of the students taking this course. -> done. Data taken from Canvas
+
+The data will be used to send updates regarding the classes. -> done. Phone Number and email
+
+There is also a need to know the progress of the students during the progress of this course. -> done.
+
+
+
+List down all data that can be used to implement this. Use the proper data types.
+ */
+
+fun main() {
+    //Student Names
+    val student1: String = "Bernard Corpuz Aglipay"
+    val student2: String = "Vismar Jay Suaybaguio Alfaro"
+    val student3: String = "Jason Almonte"
+    val student4: String = "Janreign Tio Aragon"
+    val student5: String = "Noe Pahila Arca"
+    val student6: String = "John Rey Medina Balais"
+    val student7: String = "Joni James Ramirez Dayucos"
+    val student8: String = "Ebrahim Diangca Diangca"
+    val student9: String = "Laurence De Dios"
+    val student10: String = "Ma Mickaella Rica Cabrera Fernandez"
+    val student11: String = "Patricia Ivee Leones Leones"
+    val student12: String = "Maria Patricia Calinaya Locquiao"
+    val student13: String = "Mark Lester Bismanos Del Monte"
+    val student14: String = "Matthew Alex Lander Motos"
+    val student15: String = "James Dave Esteban Navor"
+    val student16: String = "Joel Olayta Panarigan"
+    val student17: String = "Rene Mabasa Parma"
+    val student18: String = "Ryan Parpa"
+    val student19: String = "Romie Quijote Quisido"
+    val student20: String = "Mary Jean Altamero Ranola"
+    val student21: String = "James Nico Rara"
+    val student22: String = "Ronnel Remedios"
+    val student23: String = "Rey Brezuela Saliot"
+    val student24: String = "Gino Lapitan Delos Santos"
+    val student25: String = "John Patrick Macaway Soriano"
+    val student26: String = "Juan Carlos San Jose Sumaya"
+    val student27: String = "Carlo Jay A Tronco"
+    val student28: String = "Victor Marana Yu"
+
+    //Student Emails
+    var studentEMail1: String? = null
+    var studentEMail2: String? = null
+    var studentEMail3: String? = null
+    var studentEMail4: String? = null
+    var studentEMail5: String? = null
+    var studentEMail6: String? = null
+    var studentEMail7: String? = null
+    var studentEMail8: String? = null
+    var studentEMail9: String? = null
+    var studentEMail10: String? = null
+    var studentEMail11: String? = null
+    var studentEMail12: String? = null
+    var studentEMail13: String? = null
+    var studentEMail14: String? = null
+    var studentEMail15: String? = null
+    var studentEMail16: String? = null
+    var studentEMail17: String? = null
+    var studentEMail18: String? = null
+    var studentEMail19: String? = null
+    var studentEMail20: String? = null
+    var studentEMail21: String? = null
+    var studentEMail22: String? = "remediosronnel30@gmail.com"
+    var studentEMail23: String? = null
+    var studentEMail24: String? = null
+    var studentEMail25: String? = null
+    var studentEMail26: String? = null
+    var studentEMail27: String? = null
+    var studentEMail28: String? = "vicyu12@gmail.com"
+
+    //Student Contact Numbers
+    var studentContact1: String? = null
+    var studentContact2: String? = null
+    var studentContact3: String? = null
+    var studentContact4: String? = null
+    var studentContact5: String? = null
+    var studentContact6: String? = null
+    var studentContact7: String? = null
+    var studentContact8: String? = null
+    var studentContact9: String? = null
+    var studentContact10: String? = null
+    var studentContact11: String? = null
+    var studentContact12: String? = null
+    var studentContact13: String? = null
+    var studentContact14: String? = null
+    var studentContact15: String? = null
+    var studentContact16: String? = null
+    var studentContact17: String? = null
+    var studentContact18: String? = null
+    var studentContact19: String? = null
+    var studentContact20: String? = null
+    var studentContact21: String? = null
+    var studentContact22: String? = null
+    var studentContact23: String? = null
+    var studentContact24: String? = null
+    var studentContact25: String? = null
+    var studentContact26: String? = null
+    var studentContact27: String? = null
+    var studentContact28: String? = null
+
+    //Student Progress
+    var studentProgress1: String? = null
+    var studentProgress2: String? = null
+    var studentProgress3: String? = null
+    var studentProgress4: String? = null
+    var studentProgress5: String? = null
+    var studentProgress6: String? = null
+    var studentProgress7: String? = null
+    var studentProgress8: String? = null
+    var studentProgress9: String? = null
+    var studentProgress10: String? = null
+    var studentProgress11: String? = null
+    var studentProgress12: String? = null
+    var studentProgress13: String? = null
+    var studentProgress14: String? = null
+    var studentProgress15: String? = null
+    var studentProgress16: String? = null
+    var studentProgress17: String? = null
+    var studentProgress18: String? = null
+    var studentProgress19: String? = null
+    var studentProgress20: String? = null
+    var studentProgress21: String? = null
+    var studentProgress22: String? = null
+    var studentProgress23: String? = null
+    var studentProgress24: String? = null
+    var studentProgress25: String? = null
+    var studentProgress26: String? = null
+    var studentProgress27: String? = null
+    var studentProgress28: String? = null
+}
