f3d38531ea8df0c4b20d90777680032c1e13b801
Victor Yu Jr <vicyu12@gmail.com>
2022-10-10 22:44:30 +0800

    Version 2 (Accepting 2 decimals for monetary value)

--- src/main/kotlin/activity_01d/Main.kt
+++ src/main/kotlin/activity_01d/Main.kt
@@ -1,18 +1,29 @@
 package activity_01d
 
 fun main() {
+
+    //-----INTRO-----
     println("You will need to enter 5 monetary amounts that will be added together.")
     println("The sum of the 5 amounts you entered will then be divided by an integer you will input.")
     println()
 
+    //-----VARIABLES-----
     var monetaryAmount1: String? = null
     var monetaryAmount2: String? = null
     var monetaryAmount3: String? = null
     var monetaryAmount4: String? = null
     var monetaryAmount5: String? = null
     var dividedByThis: String? = null
-    var answer: Int? = null
+    var answer: Double? = null
 
+    var numericCheck1: Boolean? = null
+    var numericCheck2: Boolean? = null
+    var numericCheck3: Boolean? = null
+    var numericCheck4: Boolean? = null
+    var numericCheck5: Boolean? = null
+    var intCheck: Boolean? = null
+
+    //-----INPUTS-----
     print("Please enter the first monetary amount: ")
     monetaryAmount1 = readLine()
     print("Please enter the second monetary amount: ")
@@ -27,11 +38,20 @@ fun main() {
     print("Divide the value by how many? ")
     dividedByThis = readLine()
 
+    //-----CHECK IF MONETARY AMOUNT AND DIVIDED BY INT-----
+    numericCheck1 = monetaryAmount1?.matches("-?\\d+(\\.\\d{2})?".toRegex())
+    numericCheck2 = monetaryAmount2?.matches("-?\\d+(\\.\\d{2})?".toRegex())
+    numericCheck3 = monetaryAmount3?.matches("-?\\d+(\\.\\d{2})?".toRegex())
+    numericCheck4 = monetaryAmount4?.matches("-?\\d+(\\.\\d{2})?".toRegex())
+    numericCheck5 = monetaryAmount5?.matches("-?\\d+(\\.\\d{2})?".toRegex())
+    intCheck = dividedByThis?.matches("-?\\d".toRegex())
+
+    //-----ERROR HANDLING-----
     if (monetaryAmount1 == null || monetaryAmount2 == null || monetaryAmount3 == null || monetaryAmount4 == null || monetaryAmount5 == null || dividedByThis == null || monetaryAmount1 == "" || monetaryAmount2 == "" || monetaryAmount3 == "" || monetaryAmount4 == "" || monetaryAmount5 == "" || dividedByThis == "") {
         println("There was something missing in your input.")
-    } else if (monetaryAmount1.all {Char -> Char.isDigit()} && monetaryAmount2.all {Char -> Char.isDigit()} && monetaryAmount3.all {Char -> Char.isDigit()} && monetaryAmount4.all {Char -> Char.isDigit()} && monetaryAmount5.all {Char -> Char.isDigit()} && dividedByThis.all {Char -> Char.isDigit()}) {
-        answer = (monetaryAmount1.toInt() + monetaryAmount2.toInt() + monetaryAmount3.toInt() + monetaryAmount4.toInt() + monetaryAmount5.toInt()) / dividedByThis.toInt()
+    } else if (numericCheck1 == true && numericCheck2 == true && numericCheck3 == true && numericCheck4 == true && numericCheck5 == true && intCheck == true) {
+        answer = (monetaryAmount1.toDouble() + monetaryAmount2.toDouble() + monetaryAmount3.toDouble() + monetaryAmount4.toDouble() + monetaryAmount5.toDouble()) / dividedByThis.toDouble()
         println("The answer is $answer.")
-    } else println("Not all inputs were numbers.")
+    } else println("Either not all inputs were numbers, or your numerical input was wrong.")
 
 }
