f9dd4ff07455fe6f6006399d01d47035253fd7b4
Victor Yu Jr <vicyu12@gmail.com>
2022-10-13 17:52:46 +0800

    fixes

--- src/main/kotlin/activity_01b/Main.kt
+++ src/main/kotlin/activity_01b/Main.kt
@@ -16,15 +16,18 @@ fun main() {
     val book1: String = "A Book About Algebra"
 //    val book1DeweyDecimal: String = "512"
     var book1Availability: Boolean = true
-    var book1Characteristics: String? = "Reader friendly"
+    val book1Author: String? = "Author1"
+    val book1Publisher: String? = "Publisher1"
 
     val book2: String = "A French Dictionary"
 //    val book2DeweyDecimal: String = "443"
     val book2Availability: Boolean = true
-    var book2Characteristics: String? = "Brand new"
+    val book2Author: String? = "Author2"
+    val book2Publisher: String? = "Publisher2"
 
     val book3: String = "A Book on Religion"
 //    val book3DeweyDecimal: String = "200"
     val book3Availability: Boolean = true
-    var book3Characteristics: String? = null
+    val book3Author: String? = "Author3"
+    val book3Publisher: String? = "Publisher3"
 }
