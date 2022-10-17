fb4de1b5dbbcf7b1b67e99f0ae0eb986097a31db
Victor Yu Jr <vicyu12@gmail.com>
2022-10-17 19:18:26 +0800

    Changed all values to null

--- src/main/kotlin/activity_01c/Main.kt
+++ src/main/kotlin/activity_01c/Main.kt
@@ -11,28 +11,27 @@ You are also tasked to group the items in categories to help manage the grocery.
  */
 
 fun main() {
-    val itemName1: String = "Chicken Nuggets"
-    var itemQuantity1: Int = 25
-    var itemAvailability1: Boolean = true
+    var itemName1: String? = null
+    var itemQuantity1: Int? = null
+    var itemAvailability1: Boolean? = null
     var itemCharacteristics1: String? = null
-    val itemCategory1: String = "Frozen Food"
-    val itemBrand1: String = "PureFoods"
-    val itemModel1: String = "Crazy Cut Nuggets"
+    var itemCategory1: String? = null
+    var itemBrand1: String? = null
+    var itemModel1: String? = null
 
-    val itemName2: String = "Corned Beef"
-    var itemQuantity2: Int = 5
-    var itemAvailability2: Boolean = true
-    var itemCharacteristics2: String? = "Dented"
-    val itemCategory2: String = "Canned Food"
-    val itemBrand2: String = "PureFoods"
-    val itemModel2: String = "Chili Garlic"
+    var itemName2: String? = null
+    var itemQuantity2: Int? = null
+    var itemAvailability2: Boolean? = null
+    var itemCharacteristics2: String? = null
+    var itemCategory2: String? = null
+    var itemBrand2: String? = null
+    var itemModel2: String? = null
 
-    val itemName3: String = "Bathroom Tissue"
-    var itemQuantity3: Int = 10
-    var itemAvailability3: Boolean = true
-    var itemCharacteristics3: String? = "Sealed"
-    val itemCategory3: String = "Paper Goods"
-    val itemBrand3: String = "Sanicare"
-    val itemModel3: String = "EcoLayers"
-
+    var itemName3: String? = null
+    var itemQuantity3: Int? = null
+    var itemAvailability3: Boolean? = null
+    var itemCharacteristics3: String? = null
+    var itemCategory3: String? = null
+    var itemBrand3: String? = null
+    var itemModel3: String? = null
 }
