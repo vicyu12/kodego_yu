1497e46b5ff86bb0446bfc2135933926a45f2ea9
Victor Yu Jr <vicyu12@gmail.com>
2022-10-19 22:58:56 +0800

    Initial Commit

---
+++ src/main/kotlin/activity_01_g/Main.kt
@@ -0,0 +1,32 @@
+package activity_01_g
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun main() {
+    var string: String? = null
+    var palindrome = ArrayList<Boolean>()
+    var palindromeCounter: Int? = null
+    var palindromeFinal: Boolean = true
+
+    logger.info { "Please enter a string:" }
+    string = readLine()
+    if (string == null || string == "") {
+        logger.error { "You did not input anything." }
+    } else {
+        for (index in 0 until string.length/2)
+        {
+            palindromeCounter = string.length - index -1
+            palindrome.add(string[index].lowercaseChar() == string[palindromeCounter].lowercaseChar())
+        }
+        for (index in palindrome.indices) {
+            palindromeFinal = palindromeFinal && palindrome[index]
+        }
+    }
+    if (palindromeFinal) {
+        logger.info { "Palindrome!" }
+    } else {
+        logger.info {"This is not a Palindrome."}
+    }
+}
