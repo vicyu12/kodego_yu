27561eb44b4940c199dbd777666e2c7b9417efb4
Victor Yu Jr <vicyu12@gmail.com>
2022-10-19 22:23:25 +0800

    Initial Commit Act1F

---
+++ src/main/kotlin/activity_01f/Main.kt
@@ -0,0 +1,66 @@
+package activity_01f
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+fun main() {
+    /*
+    Create an application that will accept 2 string inputs.
+    Your application will print all unique characters in both strings.
+     */
+
+    var string1: String? = null
+    var string2: String? = null
+    val alphabet = ArrayList<Char>()
+    val letterCount = ArrayList<Int>()
+    val uniqueCharacters = ArrayList<Char>()
+    var char: Char? = null
+
+    logger.info { "Enter a first string:" }
+    string1 = readLine()
+    logger.info { "Enter another string:" }
+    string2 = readLine()
+
+    if (string1 == null || string2 == null) {
+        logger.error { "You were missing an input." }
+    } else {
+        for (index in string1.indices) {
+            char = string1[index].uppercaseChar()
+            if (alphabet.isEmpty()) {
+                alphabet.add(char)
+                letterCount.add(1)
+            } else {
+                for (letterIndex in alphabet.indices) {
+                    if (char == alphabet[letterIndex]) letterCount[letterIndex]++
+                    else if (char !in alphabet){
+                        alphabet.add(char)
+                        letterCount.add(1)
+                    }
+                }
+            }
+        }
+        for (index in string2.indices) {
+            char = string2[index].uppercaseChar()
+            if (alphabet.isEmpty()) {
+                alphabet.add(char)
+                letterCount.add(1)
+            } else {
+                for (letterIndex in alphabet.indices) {
+                    if (char == alphabet[letterIndex]) letterCount[letterIndex]++
+                    else if (char !in alphabet){
+                        alphabet.add(char)
+                        letterCount.add(1)
+                    }
+                }
+            }
+        }
+    }
+    for (index in alphabet.indices) {
+        if (letterCount[index] == 1) {
+            uniqueCharacters.add(alphabet[index])
+        }
+    }
+    for (index in uniqueCharacters.indices) {
+        logger.info { "Unique character found: ${uniqueCharacters[index]}" }
+    }
+}
