9a5f8170b5373426c0da532c76c18c714270ae17
Victor Yu Jr <vicyu12@gmail.com>
2022-11-22 23:13:56 +0800

    Initial Commit

---
+++ src/main/kotlin/activity_04c/Grocery.kt
@@ -0,0 +1,168 @@
+package activity_04c
+
+class Poultry {
+    var brand: String = ""
+    var weight: Double = 0.0
+    var pricePerKilo: Double = 0.0
+    var type: String = ""
+    var quantity: Double = 0.0
+}
+
+class Fish {
+    var brand: String = ""
+    var weight: Double = 0.0
+    var pricePerKilo: Double = 0.0
+    var type: String = ""
+    var quantity: Double = 0.0
+    var freshWater: Boolean = true
+}
+
+class CannedGoods {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class Snacks {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class FrozenProducts {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class Fruits {
+    var brand: String = ""
+    var weight: Double = 0.0
+    var pricePerKilo: Double = 0.0
+    var type: String = ""
+    var quantity: Double = 0.0
+}
+
+class Vegetables {
+    var brand: String = ""
+    var weight: Double = 0.0
+    var pricePerKilo: Double = 0.0
+    var type: String = ""
+    var quantity: Double = 0.0
+}
+
+class Condiments {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class Furniture {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class Appliances {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class Clothes {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+}
+
+class Pet {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class SanitaryProducts {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class PersonalCare {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class Sodas {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class Chips {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class Wheat {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class Dairy {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
+
+class CigarettesAndVapes {
+    var type: String = ""
+    var brand: String = ""
+    var variety: String = ""
+    var size: String = ""
+    var price: Double = 0.0
+    var name: String = ""
+}
